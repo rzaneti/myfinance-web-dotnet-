867097dbe91fbf0f323a60864ed1620a690c0ff0
Rafaela Zanetti <rafaelazanetti79@gmail.com>
2023-03-09 19:00:27 -0300

    Teste

---
+++ src/myfinance-web-dotnet-/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_.Models
+{
+    public class HomeModel
+    {
+        public string Nome {get; set;}
+        
+    }
+}
